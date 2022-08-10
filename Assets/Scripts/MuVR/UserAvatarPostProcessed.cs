e169b9bc3b19ad0c3f6625dac1b9b0c0bcb38609
Joshua Dahl <joshuadahl@nevada.unr.edu>
2022-08-10 11:18:14 -0700

    PostProcessing Modes
    The post processor now supports selectively choosing what data is processed, what data is simply copied and what data is ignored.

--- Assets/Scripts/MuVR/UserAvatarPostProcessed.cs
+++ Assets/Scripts/MuVR/UserAvatarPostProcessed.cs
@@ -1,26 +1,55 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.Scripting;
 
 namespace MuVR {
 	public abstract class UserAvatarPostProcessed : UserAvatar {
-		protected readonly Dictionary<string, PoseRef> rawSlotData = new();
+		public struct PostProcessData {
+			public enum ProcessMode {
+				Process,
+				Copy,
+				Ignore,
+			}
+			public ProcessMode processMode;
+			public readonly PoseRef poseRef;
 
-		[Preserve]
+			public PostProcessData(ProcessMode processMode) {
+				this.processMode = processMode;
+				poseRef = new PoseRef();
+			}
+		}
+		
+		protected readonly Dictionary<string, PostProcessData> rawSlotData = new();
+		
 		public void Awake() {
 			foreach (var slot in slots.Keys) 
-				rawSlotData[slot] = new PoseRef();
+				rawSlotData[slot] = new PostProcessData(PostProcessData.ProcessMode.Process);
+		}
+		
+		public override PoseRef GetterPoseRef(string slot) => slots[slot];
+		public override PoseRef SetterPoseRef(string slot) => rawSlotData[slot].poseRef;
+		
+		public virtual ref Pose GetProcessedPose(string slot) => ref GetterPoseRef(slot).pose;
+		public virtual ref Pose GetRawPose(string slot) => ref SetterPoseRef(slot).pose;
+		
+		public virtual bool ShouldProcess(string slot) => rawSlotData[slot].processMode == PostProcessData.ProcessMode.Process;
+		public virtual bool ShouldCopy(string slot) => rawSlotData[slot].processMode == PostProcessData.ProcessMode.Copy;
+		public virtual bool ShouldIgnore(string slot) => rawSlotData[slot].processMode == PostProcessData.ProcessMode.Ignore;
+		public void SetProcessMode(string slot, PostProcessData.ProcessMode value = PostProcessData.ProcessMode.Process) {
+			var data = rawSlotData[slot];
+			data.processMode = value;
+			rawSlotData[slot] = data;
 		}
 
-		public override PoseRef GetterPoseRef(string slot) => slots[slot];
-		public override PoseRef SetterPoseRef(string slot) => rawSlotData[slot];
-		
-		public ref Pose GetProcessedPose(string slot) => ref GetterPoseRef(slot).pose;
-		public ref Pose GetRawPose(string slot) => ref SetterPoseRef(slot).pose;
-
 		public void Update() {
-			foreach (var slot in rawSlotData.Keys) 
+			// Process all of the data that should be processed
+			foreach (var slot in rawSlotData.Keys.Where(ShouldProcess)) 
 				GetProcessedPose(slot) = OnPostProcess(slot, GetProcessedPose(slot), GetRawPose(slot));
+
+			// Copy all of the data that should be copied
+			foreach (var slot in rawSlotData.Keys.Where(ShouldCopy))
+				GetProcessedPose(slot) = GetRawPose(slot);
 		}
 
 		// Function that can be overridden in derived classes to process the data in some way
