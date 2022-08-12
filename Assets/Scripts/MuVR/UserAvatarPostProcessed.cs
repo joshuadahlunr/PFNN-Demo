cddc6bbe9f34cb6790ddfba411dd81d68c9993c4
Joshua Dahl <joshuadahl@nevada.unr.edu>
2022-08-11 18:01:02 -0700

    Leaky Integrator Time Scaling
    Adjust the WeightedUserAvatar (and its parents) so that the blending is performed framerate independently.

--- Assets/Scripts/MuVR/UserAvatarPostProcessed.cs
+++ Assets/Scripts/MuVR/UserAvatarPostProcessed.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using Unity.Collections;
@@ -33,6 +32,7 @@ namespace MuVR {
 		
 		#region Types
 
+		// Pose Reference and Process Mode stored in the dictionary 
 		public struct PostProcessData {
 			public enum ProcessMode {
 				Process,
@@ -48,20 +48,22 @@ namespace MuVR {
 			}
 		}
 
+		// Unity job that applies post processing
 		protected struct PostProcessJob : IJobParallelFor {
 			public uint ownerID; // The ID of the UserAvatar in the static references
-			public NativeArray<byte>.ReadOnly keyData;
-			public NativeArray<long>.ReadOnly keyStarts;
+			public float deltaTime;
+			public NativeArray<byte>.ReadOnly slotData;
+			public NativeArray<long>.ReadOnly slotStarts;
 
 			public void Execute(int index) {
-				var start = (int)keyStarts[index];
-				var end = (int)(index < keyStarts.Length - 1 ? keyStarts[index + 1] : keyData.Length);
+				var start = (int)slotStarts[index];
+				var end = (int)(index < slotStarts.Length - 1 ? slotStarts[index + 1] : slotData.Length);
 				
-				var slot = Encoding.UTF8.GetString(keyData.Skip(start).Take(end - start).ToArray());
+				var slot = Encoding.UTF8.GetString(slotData.Skip(start).Take(end - start).ToArray());
 				var owner = UserAvatarPostProcessed.inScene[ownerID];
 				
 				if(owner.ShouldProcess(slot))
-					owner.GetProcessedPose(slot) = owner.OnPostProcess(slot, owner.GetProcessedPose(slot), owner.GetRawPose(slot));
+					owner.GetProcessedPose(slot) = owner.OnPostProcess(slot, owner.GetProcessedPose(slot), owner.GetRawPose(slot), deltaTime);
 				else if(owner.ShouldCopy(slot))
 					owner.GetProcessedPose(slot) = owner.GetRawPose(slot);
 			}
@@ -69,27 +71,44 @@ namespace MuVR {
 
 		#endregion
 
-		// Weather or not post processing should be performed using jobs or sequentially 
+		[Tooltip("Weather or not post processing should be performed using jobs or sequentially")] 
 		public bool useJobs = true;
 		
+		// Additional dictionary that contains the raw pose data before it has been processed
 		protected readonly Dictionary<string, PostProcessData> rawSlotData = new();
+		
+		// Native arrays 
+		protected NativeArray<byte> slotsNativeArray;
+		protected NativeArray<long> startsNativeArray;
+		// Handle to the job which so it can be awaited at the end of the frame
+		protected JobHandle postProcessJob;
+		// When the object goes away cleanup the native arrays
+		public void OnDestroy() {
+			if (slotsNativeArray.IsCreated) slotsNativeArray.Dispose();
+			if (startsNativeArray.IsCreated) startsNativeArray.Dispose();
+		}
 
+		// When the object is created "copy" (+ modifications) the pose-slot dictionary from the base class
 		public void Awake() {
 			foreach (var slot in slots.Keys) 
 				rawSlotData[slot] = new PostProcessData(PostProcessData.ProcessMode.Process);
 		}
-
-		public void OnValidate() {
+		// In the editor make sure the there is valid data for UI updates
+#if UNITY_EDITOR
+		protected void OnValidate() {
 			rawSlotData.Clear();
 			Awake();
 		}
-
+#endif
+		
 		public override PoseRef GetterPoseRef(string slot) => slots[slot];
 		public override PoseRef SetterPoseRef(string slot) => rawSlotData[slot].poseRef;
 		
+		// Access to the processed and raw pose information
 		public virtual ref Pose GetProcessedPose(string slot) => ref GetterPoseRef(slot).pose;
 		public virtual ref Pose GetRawPose(string slot) => ref SetterPoseRef(slot).pose;
 		
+		// Access to data indicating how the data in a given slot should be handled
 		public virtual bool ShouldProcess(string slot) => rawSlotData[slot].processMode == PostProcessData.ProcessMode.Process;
 		public virtual bool ShouldCopy(string slot) => rawSlotData[slot].processMode == PostProcessData.ProcessMode.Copy;
 		public virtual bool ShouldIgnore(string slot) => rawSlotData[slot].processMode == PostProcessData.ProcessMode.Ignore;
@@ -99,38 +118,31 @@ namespace MuVR {
 			rawSlotData[slot] = data;
 		}
 
-		// Native arrays 
-		protected NativeArray<byte> keysNativeArray;
-		protected NativeArray<long> startsNativeArray;
-		protected JobHandle postProcessJob;
-		public void OnDestroy() {
-			if (keysNativeArray.IsCreated) keysNativeArray.Dispose();
-			if (startsNativeArray.IsCreated) startsNativeArray.Dispose();
-		}
-
+		// Perform the data processing every frame...
 		public void Update() {
 			#region Jobs
 
 			if (useJobs) {
 				// If the old arrays are dirty
-				if (!keysNativeArray.IsCreated || !startsNativeArray.IsCreated || startsNativeArray.Length != rawSlotData.Count) {
+				if (!slotsNativeArray.IsCreated || !startsNativeArray.IsCreated || startsNativeArray.Length != rawSlotData.Count) {
 					// Dispose of the old arrays (if they exists)
-					if (keysNativeArray.IsCreated)
-						keysNativeArray.Dispose();
+					if (slotsNativeArray.IsCreated)
+						slotsNativeArray.Dispose();
 					if (startsNativeArray.IsCreated)
 						startsNativeArray.Dispose();
 
-					// Allocate a new arrays
+					// Allocate new arrays
 					rawSlotData.Keys.Select(s => Encoding.UTF8.GetBytes(s)).Concatentate(out var data, out var starts);
-					keysNativeArray = new NativeArray<byte>(data, Allocator.Persistent);
+					slotsNativeArray = new NativeArray<byte>(data, Allocator.Persistent);
 					startsNativeArray = new NativeArray<long>(starts, Allocator.Persistent);
 				}
 
 				postProcessJob = new PostProcessJob {
 					ownerID = indexInScene,
-					keyData = keysNativeArray.AsReadOnly(),
-					keyStarts = startsNativeArray.AsReadOnly()
-				}.Schedule(rawSlotData.Count, 1); //.Complete();
+					deltaTime = Time.deltaTime,
+					slotData = slotsNativeArray.AsReadOnly(),
+					slotStarts = startsNativeArray.AsReadOnly()
+				}.Schedule(rawSlotData.Count, 1); // Job is completed in LateUpdate
 			}
 
 			#endregion
@@ -142,7 +154,7 @@ namespace MuVR {
 			{
 				// Process all of the data that should be processed
 				foreach (var slot in rawSlotData.Keys.Where(ShouldProcess))
-					GetProcessedPose(slot) = OnPostProcess(slot, GetProcessedPose(slot), GetRawPose(slot));
+					GetProcessedPose(slot) = OnPostProcess(slot, GetProcessedPose(slot), GetRawPose(slot), Time.deltaTime);
 
 				// Copy all of the data that should be copied
 				foreach (var slot in rawSlotData.Keys.Where(ShouldCopy))
@@ -156,6 +168,6 @@ namespace MuVR {
 		public void LateUpdate() => postProcessJob.Complete();
 
 		// Function that can be overridden in derived classes to process the data in some way
-		public abstract Pose OnPostProcess(string slot, Pose processed, Pose raw);
+		public abstract Pose OnPostProcess(string slot, Pose processed, Pose raw, float dt);
 	}
 }
--- Assets/Scripts/WeightedUserAvatar.cs
+++ Assets/Scripts/WeightedUserAvatar.cs
@@ -5,11 +5,22 @@ namespace MuVR {
 	public class WeightedUserAvatar : UserAvatarPostProcessed {
 		public float positionAlpha = .9f;
 		public float rotationAlpha = .9f;
+		public float timeScale = 60;
 
-		public override Pose OnPostProcess(string slot, Pose smoothed, Pose unsmoothed) {
-			smoothed.position = positionAlpha * smoothed.position + (1 - positionAlpha) * unsmoothed.position;
-			smoothed.rotation = Quaternion.Slerp(smoothed.rotation, unsmoothed.rotation, 1 - rotationAlpha);
-			return smoothed;
+		public override Pose OnPostProcess(string slot, Pose smoothed, Pose unsmoothed, float dt) {
+			var modified = new Pose {
+				position = positionAlpha * smoothed.position + (1 - positionAlpha) * unsmoothed.position,
+				rotation = Quaternion.Slerp(smoothed.rotation, unsmoothed.rotation, 1 - rotationAlpha)
+			};
+			// Preform the blending with respect to time
+			return Lerp(smoothed, modified, dt * timeScale);
+		}
+		
+		public static Pose Lerp(Pose a, Pose b, float t) {
+			Pose ret;
+			ret.position = Vector3.Lerp(a.position, b.position, t);
+			ret.rotation = Quaternion.Lerp(a.rotation, b.rotation, t);
+			return ret;
 		}
 	}
 }
